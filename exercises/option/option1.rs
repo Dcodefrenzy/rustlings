8dcc4600220810281c1a62b9faf115fd8f8a4962
Ayodeji <ayodejisamuelfakunle@gmail.com>
2022-01-11 00:07:23 +0100

    fixed the Option enum error

--- exercises/option/option1.rs
+++ exercises/option/option1.rs
@@ -14,12 +14,12 @@ fn main() {
     print_number(num1);
     print_number(num2);
 
-    let numbers: [Option<u16>; 5];
+    let mut numbers: [Option<u16>; 5] = [None; 5];
     for iter in 0..5 {
-        let number_to_add:  Option<u16> = {
-            Some(((iter * 1235) + 2) / (4 * 16))
+        let number_to_add: u16 = {
+            ((iter * 1235) + 2) / (4 * 16)
         };
 
-        numbers[iter as usize] = number_to_add;
+        numbers[iter as usize] = Some(number_to_add);
     }
 }
